67dfcba97b9dd6c17f79d0fddd8bd3ac4e419cee
Martin Filliau <martin.filliau@gmail.com>
2010-12-04 10:06:14 +0000

    initial commit

---
+++ kindle2rdf.pl
@@ -0,0 +1,15 @@
+# kindle2rdf
+# Convert Kindle clippings in RDF
+# Martin Filliau, 2010
+
+print
+    "<rdf:RDF
+	xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+    	xmlns:rdfs='http://www.w3.org/2000/01/rdf-schema#'>
+";
+
+
+
+
+print "</rdf:RDF>\n";
+
